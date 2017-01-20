69db92559b9b2f79b00822533507c07ead23943c
HamiltonSantana <hamilton.santana@outlook.com>
2017-01-20 07:14:38 -0200

    Atualização no Setup, Caixa e exemploes para Abas e Combobox

--- caixa.py
+++ caixa.py
@@ -13,56 +13,63 @@ class Caixa(Frame):
 		frameTitulo = Frame(self)
 		frameTitulo.pack(fill=X)
 
-		""" Criar Menu """
+		"""  Criar Menu """
+		menubar = Menu(frameTitulo)
+		filMenu = Menu(menubar, tearoff=0)
+		filMenu.add_command(label="Open")
+		filMenu.add_command(label="Save")
+		filMenu.add_separator()
+		filMenu.add_command(label="Exit")
+		menubar.add_cascade(label="File", menu=filMenu)
+		""" Menu 2 """
+		editMenu = Menu(menubar, tearoff=0)
+		editMenu.add_command(label="Cut")
+		editMenu.add_command(label="Copy")
+		editMenu.add_command(label="Paste")
+		menubar.add_cascade(label="Edit", menu=editMenu)
+		""" Menu 3 """ 
+		opMenu = Menu(menubar, tearoff=0)
+		opMenu.add_command(label="Opcao")
+		menubar.add_cascade(label="Option", menu=opMenu)
+		
+		prefMenu = Menu(menubar, tearoff=0)
+		prefMenu.add_command(label="Configuraçoes")
+		menubar.add_cascade(label="Preferencia", menu=prefMenu)
 
-		lb_menu = Label(frameTitulo, text="Menu")
-		lb_menu.pack(side=LEFT, padx=5, pady=5)		
-		lb_tool = Label(frameTitulo, text="Ferramentas")
-		lb_tool.pack(side=LEFT, padx=5, pady=5)
-		lb_arq= Label(frameTitulo, text="Arquivos")
-		lb_arq.pack(side=LEFT, padx=5, pady=5)
-		lb_opcao = Label(frameTitulo, text="Opcoes")
-		lb_opcao.pack(side=LEFT, padx=5, pady=5)
-		lb_preferencia = Label(frameTitulo, text="Preferencias")
-		lb_preferencia.pack(side=LEFT, padx=5, pady=5)
-
+		helpMenu = Menu(menubar, tearoff=0)
+		helpMenu.add_command(label="About")
+		menubar.add_cascade(label="Help",menu=helpMenu)
+				
+		self.parent.config(menu=menubar)
+		
 		frameCad = Frame(self)
+		frameCad.columnconfigure(0, weight=1)
 		frameCad.pack(fill=X)
-
-		""" Etiquetas """
+		
+		""" Declara entradas """
 		lb_nome = Label(frameCad, text="Nome do cliente:")
-		lb_nome.pack(side=LEFT, padx=5, pady=5)
+		lb_placa = Label(frameCad, text="Placa do carro:")
+		lb_modelo = Label(frameCad, text="Modelo do carro:")
+		lb_fabri = Label(frameCad, text="Fabricante: ")
+		
 		en_nome = Entry(frameCad)
-		en_nome.pack(fill=X, padx=5, expand=True)
-
-		frameCad2 = Frame(self)
-		frameCad2.pack(fill=X)
-
-		lb_placa = Label(frameCad2, text="Placa do carro:")
-		en_placa = Entry(frameCad2)
-		lb_placa.pack(side=LEFT, padx=5, pady=5)
-		en_placa.pack(fill=X, padx=5, expand=True)
-
-		frameCad3 = Frame(self)
-		frameCad3.pack(fill=X)
-
-		lb_modelo = Label(frameCad3, text="Modelo do carro:")
-		en_modelo = Entry(frameCad3)
-		lb_modelo.pack(side=LEFT, padx=5, pady=5)
-		en_modelo.pack(fill=X, padx=5, expand=True)
-
-		frameCad4 = Frame(self)
-		frameCad4.pack(fill=X)
-
-		lb_fabri = Label(frameCad4, text="Fabricante: ")
-		en_fabri = Entry(frameCad4)
-		lb_fabri.pack(side=LEFT, padx=5, pady=5)
-		en_fabri.pack(fill=X, padx=5, expand=True)
-
+		en_placa = Entry(frameCad)
+		en_modelo = Entry(frameCad)
+		en_fabri = Entry(frameCad)
+		""" Packing entradas """
+		lb_nome.grid(row=0, column=0, sticky=W)
+		en_nome.grid(row=1, sticky=E+W)
+		lb_placa.grid(row=2, column=0, sticky=W)
+		en_placa.grid(row=3, sticky=E+W)
+		lb_modelo.grid(row=4, column=0, sticky=W)
+		en_modelo.grid(row=5, sticky=E+W)
+		lb_fabri.grid(row=6, column=0, sticky=W)
+		en_fabri.grid(row=7, sticky=E+W)	
+		
 def main():
 
 	root = Tk()
-	root.geometry("400x200")
+	root.geometry("400x210")
 	app = Caixa(root)
 	root.mainloop()
 
